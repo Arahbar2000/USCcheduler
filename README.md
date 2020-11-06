29a065d61a49f8212a546e1dbe63313c1e88fd22
Jiashu XU <1999J0615une@gmail.com>
2020-11-05 20:42:13 -0800

    add script to create database

---
+++ README.md
@@ -0,0 +1,11 @@
+# SQL
+
+To create table 
+
+```sql
+
+mysql -u chris -p < createTable.sql
+
+```
+
+To populate table
---
+++ scripts/createTable.sql
@@ -0,0 +1,74 @@
+drop database if exists cs201;
+create database cs201;
+use cs201;
+
+drop table if exists Users;
+create table Users
+(
+	userId int auto_increment,
+	lastName varchar(50) not null,
+	firstName varchar(50) not null,
+	email varchar(50) not null,
+	password int not null,
+	constraint Users_pk
+		primary key (userId)
+);
+
+drop table if exists Course;
+create table Course
+(
+	courseId int not null,
+	department VARCHAR(50) not null,
+	courseNumber int not null,
+	startTime varchar(50) not null,
+	endTime varchar(50) not null,
+	section varchar(50) not null,
+	instructor varchar(50) not null,
+	units int not null,
+	required BOOLEAN not null,
+	daysOfWeek varchar(50) not null comment 'use string like "MW"',
+	constraint Course_pk
+		primary key (courseId)
+);
+
+
+drop table if exists Friends;
+create table Friends
+(
+	userId int not null,
+	friendId int not null,
+	constraint Friends_pk
+		primary key (userId, friendId),
+	constraint Friends_Users_userId_fk
+		foreign key (userId) references Users (userId),
+    constraint Friends_Users_friendId_fk
+        foreign key (friendId) references Users (userId)
+);
+
+
+drop table if exists Schedule;
+create table Schedule
+(
+	courseId int not null,
+	userId int not null,
+	constraint Schedule_pk
+		primary key (courseId, userId),
+	constraint Schedule_Course_courseId_fk
+		foreign key (courseId) references Course (courseId),
+	constraint Schedule_Users_userId_fk
+		foreign key (userId) references Users (userId)
+);
+
+
+drop table if exists Preferences;
+create table Preferences
+(
+	userId int not null,
+	courseName varchar(50) not null,
+	constraint Takes_pk
+		primary key (userId, courseName),
+	constraint Takes_Users_userId_fk
+		foreign key (userId) references Users (userId)
+);
+
+
