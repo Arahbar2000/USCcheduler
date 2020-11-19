be25f2ebace5ae3a65f66f9805c11f46bf834ea3
Arian Rahbar <arahbar@usc.edu>
2020-11-19 01:17:29 -0600

    scheduler shows events

--- FE/src/Components/Schedule.js
+++ FE/src/Components/Schedule.js
@@ -1,13 +1,35 @@
+import { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid'
+import { getSchedules2 } from '../Helpers/getSchedules'
 
 const Schedule = () => {
+    // useEffect(() => {
+    //     if (localStorage.getItem('schedules') == null) {
+
+    //         localStorage.setItem('schedules') = getSchedules2()
+    //     }
+    //     else {
+
+    //     }
+    // })
+    let schedules = getSchedules2();
+    console.log(schedules)
+    const [selectedSchedule, setSchedule] = useState(schedules[0])
+    console.log(selectedSchedule)
     return (
         <FullCalendar
             schedulerLicenseKey={'CC-Attribution-NonCommercial-NoDerivatives'}
             plugins={[ timeGridPlugin ]}
             initialView={'timeGridWeek'}
             headerToolbar={false}
+            initialDate={'2020-12-07'}
+            hiddenDays={[0, 6]}
+            dayHeaderFormat={{weekday: 'long'}}
+            events={selectedSchedule}
+            allDaySlot={false}
+            slotMinTime={'07:00:00'}
+            slotMaxTime={'22:00:00'}
         />
     );
 }
--- FE/src/Helpers/getSchedules.js
+++ FE/src/Helpers/getSchedules.js
@@ -1,3 +1,6 @@
+import { string } from "prop-types"
+import axios from 'axios'
+
 export const getSchedules = () => {
     // communicates with api and returns array of schedule combinations
     // each schedule is composed of a list of courses
@@ -16,7 +19,74 @@ export const getSchedules = () => {
     })
 }
 
+const DATE_CONVERTER = {
+    'M': '2020-12-08',
+    'T': '2020-12-09',
+    'W': '2020-12-10',
+    'H': '2020-12-11',
+    'F': '2020-12-12'
+}
+
+const schedulesMock = [
+    {
+        decidedClasses: [
+            {
+                department: 'CSCI',
+                courseNumber: 201,
+                daysOfWeek: 'MWF',
+                startTime: '18:00',
+                endTime: '19:00',
+                sectionType: 'Lec'
+            },
+            {
+                department: 'CSCI',
+                courseNumber: 270,
+                daysOfWeek: 'TH',
+                startTime: '15:00',
+                endTime: '18:00',
+                sectionType: 'Lec'
+            },
+            {
+                department: 'Stupid-GE',
+                courseNumber: 100,
+                daysOfWeek: 'MWF',
+                startTime: '09:00',
+                endTime: '10:00',
+                sectionType: 'Lec'
+            }
+        ]
+    }
+]
+export const getSchedules2 = () => {
+    return generateEvents(schedulesMock)
+}
+
 // helper function for getSchedules
 const generateEvents = (schedules) => {
     // parses schedules in order to convert schedules into lists of events that are able to serve as input to calendar component
+    const eventSchedules = []
+    schedules.forEach(schedule => {
+        const events = [];
+        let id = 0;
+        schedule.decidedClasses.forEach(section => {
+            for(let i = 0; i < section.daysOfWeek.length; i++) {
+                const start = new Date(DATE_CONVERTER[section.daysOfWeek[i]])
+                start.setHours(section.startTime.split(':')[0], section.startTime.split(':')[1], 0);
+                console.log(start)
+                const end = new Date(DATE_CONVERTER[section.daysOfWeek[i]])
+                end.setHours(section.endTime.split(':')[0], section.endTime.split(':')[1], 0);
+                console.log(end)
+                const title = section.department + section.courseNumber.toString() + '-' + section.sectionType;
+                events.push({
+                    id,
+                    title,
+                    start: start,
+                    end: end
+                })
+                id++;
+            }
+        })
+        eventSchedules.push(events)
+    })
+    return eventSchedules
 }
