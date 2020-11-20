dd3ecbba49371969783d864063d7c237d412b10f
Arian Rahbar <arahbar@usc.edu>
2020-11-20 17:24:11 -0600

    test

--- FinalProj201/src/main/JDBCCredential.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class JDBCCredential {
-    final static String url = "jdbc:mysql://localhost:3306/cs201";
-    final static String username = "root";
-    final static String password = "password";
-}
