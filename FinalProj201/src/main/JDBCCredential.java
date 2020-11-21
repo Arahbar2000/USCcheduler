a8188af6d7ff2060e8c3867f44cc6e032266e744
Gursimran Bains <gsbains@usc.edu>
2020-11-20 20:24:18 -0800

    fixed visibility

--- FinalProj201/src/main/JDBCCredential.java
+++ FinalProj201/src/main/JDBCCredential.java
@@ -1,7 +1,7 @@
 package main;
 
 public class JDBCCredential {
-    final static String url = "jdbc:mysql://localhost:3306/cs201";
-    final static String username = "password";
-    final static String password = "root";
+    public final static String url = "jdbc:mysql://localhost:3306/cs201";
+    public final static String username = "name";
+    public final static String password = "pass";
 }
