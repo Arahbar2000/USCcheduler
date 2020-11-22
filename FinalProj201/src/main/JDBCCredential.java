beeb2750ba87d440f4369c0b50b29d657662b75e
Jiashu Xu <1999J0615une@gmail.com>
2020-11-22 13:20:34 -0800

    revert JDBC back

--- FinalProj201/src/main/JDBCCredential.java
+++ FinalProj201/src/main/JDBCCredential.java
@@ -2,6 +2,6 @@ package main;
 
 public class JDBCCredential {
     public final static String url = "jdbc:mysql://localhost:3306/cs201";
-    public final static String username = "chris";
-    public final static String password = "1313";
+    public final static String username = "name";
+    public final static String password = "pass";
 }
