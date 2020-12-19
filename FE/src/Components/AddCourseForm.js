2fffbe304cd581ddd8369dec849a733708d50fbc
Arian Rahbar <arahbar@usc.edu>
2020-12-18 22:18:41 -0600

    changes to addCourseForm

--- FE/src/Components/AddCourseForm.js
+++ FE/src/Components/AddCourseForm.js
@@ -5,56 +5,64 @@ import { API_URL } from '../env'
 import Select from 'react-select';
 import Button from 'react-bootstrap/Button';
 import { generateSchedules } from '../Helpers/getSchedules';
+import AsyncSelect from 'react-select/async';
+
+const queryCourses = (query, cancelToken) => {
+    console.log('getting courses')
+    if (!query) {
+        return "";
+    }
+    let department = query.match('[^0-9]+')
+    let courseNumber = query.match('[0-9]+[^0-9]*')
+    if (department != null) department = department[0].toUpperCase();
+    if (courseNumber != null) courseNumber = courseNumber[0].toUpperCase();
+    const url = new URL(API_URL + 'query');
+    url.search = new URLSearchParams({ department, courseNumber });
+    axios.get(url, {
+        cancelToken
+    }).then(response => {
+        const courses = response.data
+        const courseOptions = courses.map(course => {
+            const name = course.department.toUpperCase() + course.courseNumber.toString();
+            return {
+                label: name + ': ' + course.title,
+                value: name
+            }
+        });
+        // setOptions(courseOptions);
+        // setLoading(false);
+        return courseOptions;
+    })
+    .catch(error => {
+        if (!axios.isCancel(error)) {
+            return [];
+        }
+        return [];
+    })
+}
+
+const loadOptions = (inputValue, callback) => {
+    const { cancel, token } = axios.CancelToken.source();
+    setTimeout(() => {
+        return callback(queryCourses(inputValue, token));
+    }, 1000);
+    console.log('done getting options')
+}
+
 const AddCourseForm = props => {
     const [ query, setQuery ] = useState("");
     const [ options, setOptions ] = useState([]);
     const [ defaultOptions, setDefaultOptions ] = useState([]);
     const [ selectedCourses, setSelectedCourses ] = useState([]);
     const [ loading, setLoading ] = useState(false);
-    const attemptQuery = useRef(
-        debounce((query, token) => {
-            queryCourses(query, token);
-        }, 500)
-    ).current
 
-    // useEffect(() => {
-    //     console.log('getting default options')
-    //     let department = null;
-    //     let courseNumber = null;
-    //     const url = new URL(API_URL + 'query');
-    //     url.search = new URLSearchParams({ department, courseNumber });
-    //     axios.get(url).then(response => {
-    //         const courses = response.data
-    //         const courseOptions = courses.map(course => {
-    //             const name = course.department.toUpperCase() + course.courseNumber.toString();
-    //             return {
-    //                 label: name + ': ' + course.title,
-    //                 value: name
-    //             }
-    //         });
-    //         setDefaultOptions(courseOptions)
-    //     })
-    //     .catch(error => {
-    //         console.log('error getting default options', error)
-    //         setDefaultOptions([]);
-    //     })
-    // }, []);
-
-    const queryCourses = (query, cancelToken) => {
-        if (!query) {
-            setOptions([])
-            return;
-        }
-        setLoading(true);
-        let department = query.match('[^0-9]+')
-        let courseNumber = query.match('[0-9]+[^0-9]*')
-        if (department != null) department = department[0].toUpperCase();
-        if (courseNumber != null) courseNumber = courseNumber[0].toUpperCase();
+    useEffect(() => {
+        console.log('getting default options')
+        let department = null;
+        let courseNumber = null;
         const url = new URL(API_URL + 'query');
         url.search = new URLSearchParams({ department, courseNumber });
-        axios.get(url, {
-            cancelToken
-        }).then(response => {
+        axios.get(url).then(response => {
             const courses = response.data
             const courseOptions = courses.map(course => {
                 const name = course.department.toUpperCase() + course.courseNumber.toString();
@@ -63,28 +71,62 @@ const AddCourseForm = props => {
                     value: name
                 }
             });
-            setOptions(courseOptions);
-            setLoading(false);
+            setDefaultOptions(courseOptions)
         })
         .catch(error => {
-            if (!axios.isCancel(error)) {
-                setOptions([]);
-                setLoading(false);
-            }
+            console.log('error getting default options', error)
+            setDefaultOptions([]);
         })
-    }
+    }, []);
+
+    // const queryCourses = (query, cancelToken) => {
+    //     console.log('getting courses')
+    //     if (!query) {
+    //         setOptions([])
+    //         return "";
+    //     }
+    //     setLoading(true);
+    //     let department = query.match('[^0-9]+')
+    //     let courseNumber = query.match('[0-9]+[^0-9]*')
+    //     if (department != null) department = department[0].toUpperCase();
+    //     if (courseNumber != null) courseNumber = courseNumber[0].toUpperCase();
+    //     const url = new URL(API_URL + 'query');
+    //     url.search = new URLSearchParams({ department, courseNumber });
+    //     axios.get(url, {
+    //         cancelToken
+    //     }).then(response => {
+    //         const courses = response.data
+    //         const courseOptions = courses.map(course => {
+    //             const name = course.department.toUpperCase() + course.courseNumber.toString();
+    //             return {
+    //                 label: name + ': ' + course.title,
+    //                 value: name
+    //             }
+    //         });
+    //         // setOptions(courseOptions);
+    //         // setLoading(false);
+    //         return courseOptions;
+    //     })
+    //     .catch(error => {
+    //         if (!axios.isCancel(error)) {
+    //             setOptions([]);
+    //             setLoading(false);
+    //         }
+    //         return [];
+    //     })
+    // }
 
     const handleInputChange = input => {
         setQuery(input);
         return input;
     }
 
-    useEffect(() => {
-        const { cancel, token } = axios.CancelToken.source();
-        attemptQuery(query, token);
-        // return () => cancel("Irrelevant request") || attemptQuery.cancel();
-        return () => cancel("Irrelevant request");
-    }, [ attemptQuery, query ]);
+    // useEffect(() => {
+    //     const { cancel, token } = axios.CancelToken.source();
+    //     attemptQuery(query, token);
+    //     // return () => cancel("Irrelevant request") || attemptQuery.cancel();
+    //     return () => cancel("Irrelevant request");
+    // }, [ attemptQuery, query ]);
 
     const handleChange = courses => {
         if (courses != null) {
@@ -114,7 +156,6 @@ const AddCourseForm = props => {
         })
         .then(response => response.json())
         .then(response => {
-            const tba_courses = response.TBA
             const schedules = response.schedule
             localStorage.setItem("schedules", JSON.stringify(schedules));
             const allEvents = generateSchedules(schedules);
@@ -142,12 +183,13 @@ const AddCourseForm = props => {
 
     return (
         <div style={{margin: 'auto'}}>
-            <Select 
+            <AsyncSelect 
                 isLoading={loading} 
                 styles={styles} 
-                onChange={handleChange} 
-                onInputChange={handleInputChange} 
-                options={options} 
+                onChange={handleChange}
+                loadOptions={loadOptions}
+                // onInputChange={handleInputChange}
+                // options={options}
                 isMulti
                 placeholder='Enter course name e.g. csci201'
             />
